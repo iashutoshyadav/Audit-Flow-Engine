219afcfa9d01eae00e1c81282d58b808f03b6864
Ashutosh Yadav <ashutosh2213072@akgec.ac.in>
2026-02-15 06:30:36 +0530

    fix some issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,16 +1,24 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes.extract import router
+import os
 
 app = FastAPI(title="Financial Extractor API")
 
+frontend_url = os.getenv("FRONTEND_URL")
+
+origins = [
+    frontend_url,
+    "http://localhost:5173",
+    "http://localhost:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["*"],
 )
 
 @app.get("/")
